aff6e18cd29415d13a74c027818ea27ab0b5e491
Suzana Vukovic <suzana@vukovic.ca>
2021-11-19 17:10:35 -0500

    Setup of files

---
+++ KNN.py
@@ -0,0 +1,11 @@
+import sklearn 
+from sklearn.utils import shuffle 
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd 
+import numpy as np
+from sklearn import linear_model, preprocessing 
+
+data = pd.read_csv("car.data")
+print(data.head())
+
+le = preprocessing.LabelEncoder()
